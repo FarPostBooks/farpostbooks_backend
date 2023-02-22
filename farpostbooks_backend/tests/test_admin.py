bd7f3091fbda7fead032b9b60da2499a4b5e118c
Faner201 <62674880+Faner201@users.noreply.github.com>
2023-02-23 02:32:05 +1000

    Refactoring test admin

--- farpostbooks_backend/tests/test_admin.py
+++ farpostbooks_backend/tests/test_admin.py
@@ -34,6 +34,29 @@ async def test_get_user(
 
 
 @pytest.mark.anyio
+async def test_fail_get_user(
+    fastapi_app: FastAPI,
+    admin_client: AsyncClient,
+    fake: Faker,
+) -> None:
+    """Тест ошибки эндпоинта с проверкой существования юзера и правильной работы БД."""
+    dao = UserDAO()
+
+    telegram_id = secrets.randbelow(1000000000000)
+    await dao.create_user_model(
+        telegram_id=telegram_id,
+        name=f"{fake.first_name()} {fake.last_name()}",
+        position=fake.job(),
+        about=fake.sentence(nb_words=10),
+    )
+
+    url = fastapi_app.url_path_for("get_user", telegram_id=telegram_id - 1)
+    response = await admin_client.get(url)
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+@pytest.mark.anyio
 async def test_scopes(
     fastapi_app: FastAPI,
     client: AsyncClient,
@@ -100,3 +123,36 @@ async def test_update_user(
     assert updated_user.name == new_name
     assert updated_user.position == new_position
     assert updated_user.about == user.about
+
+
+@pytest.mark.anyio
+async def test_fail_update_user(
+    fastapi_app: FastAPI,
+    admin_client: AsyncClient,
+    fake: Faker,
+) -> None:
+    """Тест эндпоинта для обновления данных пользователя в БД."""
+    dao = UserDAO()
+
+    telegram_id = secrets.randbelow(1000000000000)
+    await dao.create_user_model(
+        telegram_id=telegram_id,
+        name=f"{fake.first_name()} {fake.last_name()}",
+        position=fake.job(),
+        about=fake.sentence(nb_words=10),
+    )
+    new_name = f"{fake.first_name()} {fake.last_name()}"
+    new_position = fake.job()
+
+    url = fastapi_app.url_path_for("update_user", telegram_id=telegram_id - 1)
+    response = await admin_client.put(
+        url,
+        json={
+            "name": new_name,
+            "position": new_position,
+        },
+    )
+    updated_user = await dao.get_user(telegram_id)
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert updated_user is None
