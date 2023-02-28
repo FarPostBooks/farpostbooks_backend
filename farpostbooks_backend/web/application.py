8125cb042e4eb889c7571a277bb8561e6d948eee
KatantDev <vlad26@mail.ru>
2023-02-28 11:29:24 +1000

    Moved origins for CORS to .env

--- farpostbooks_backend/web/application.py
+++ farpostbooks_backend/web/application.py
@@ -64,7 +64,7 @@ def get_app() -> FastAPI:
     # CORS
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
+        allow_origins=[settings.origins],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
